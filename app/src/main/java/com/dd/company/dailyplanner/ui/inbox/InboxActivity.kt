fa3871a2eb301eede5fb4edffaecbbabb2175f63
doanvu2000 <dauphong13@gmail.com>
2023-05-20 04:56:47 +0700

    fix show list inbox with status isDone

--- app/src/main/java/com/dd/company/dailyplanner/ui/inbox/InboxActivity.kt
+++ app/src/main/java/com/dd/company/dailyplanner/ui/inbox/InboxActivity.kt
@@ -1,17 +1,16 @@
 package com.dd.company.dailyplanner.ui.inbox
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
+import androidx.annotation.RequiresApi
 import com.dd.company.dailyplanner.data.PlanEntity
 import com.dd.company.dailyplanner.data.api.PlanService
 import com.dd.company.dailyplanner.data.api.RetrofitClient
 import com.dd.company.dailyplanner.databinding.ActivityInboxBinding
 import com.dd.company.dailyplanner.ui.base.BaseActivity
-import com.dd.company.dailyplanner.utils.SharePreferenceUtil
-import com.dd.company.dailyplanner.utils.enqueueShort
-import com.dd.company.dailyplanner.utils.setGridManager
-import com.dd.company.dailyplanner.utils.setOnSafeClick
+import com.dd.company.dailyplanner.utils.*
 
 class InboxActivity : BaseActivity<ActivityInboxBinding>() {
     private val apiService by lazy {
@@ -51,20 +50,41 @@ class InboxActivity : BaseActivity<ActivityInboxBinding>() {
         inboxNotDoneAdapter.setDataList(listPlan.filter { !it.isDone })
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     @SuppressLint("NotifyDataSetChanged")
     override fun initListener() {
-        binding.btnClose.setOnSafeClick { finish() }
+        binding.btnClose.setOnSafeClick { onBack() }
         binding.btnAddnewInbox.setOnSafeClick {
 
         }
         inboxDoneAdapter.onClickPlus = { item, position ->
+            item.isDone = false
             inboxNotDoneAdapter.dataList.add(item)
             inboxNotDoneAdapter.notifyDataSetChanged()
         }
         inboxNotDoneAdapter.onClickPlus = { item, position ->
+            item.isDone = true
             inboxDoneAdapter.dataList.add(item)
             inboxDoneAdapter.notifyDataSetChanged()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun onBack() {
+        listPlan.removeIf { it.type == 1 }
+        listPlan.addAll(inboxDoneAdapter.dataList)
+        listPlan.addAll(inboxNotDoneAdapter.dataList)
+        apiService.syncPlan(email, listPlan).enqueueShort(success = {
+            finish()
+        }, failed = {
+            showToast("Error when add plan: ${it.message}")
+        })
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onBackPressed() {
+        onBack()
     }
 
     override fun inflateViewBinding(inflater: LayoutInflater): ActivityInboxBinding {
